4199a3eac253e2117d6ebd15485e305e358acf25
litiancheng <litiancheng@xiaoneng.cn>
2019-09-23 14:38:31 +0800

    增加spalsh页面

--- 3.0_Vistor/src/com/biyi/hypnosis/view/HomeActivity.java
+++ 3.0_Vistor/src/com/biyi/hypnosis/view/HomeActivity.java
@@ -1,14 +1,19 @@
 package com.biyi.hypnosis.view;
 
-import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.biyi.hypnosis.R;
 
 
-public class HomeActivity extends Activity {
-
-
+public class HomeActivity extends AppCompatActivity {
+    public static void startActivity(Context context){
+           Intent intent= new Intent(context,HomeActivity.class);
+           context.startActivity(intent);
+           
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
