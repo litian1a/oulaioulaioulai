bcaca239379a22c8d86afce9aef3a5389d4d1783
litiancheng <litiancheng@xiaoneng.cn>
2019-09-23 21:32:42 +0800

    增加baseactivity方法

--- 3.0_Vistor/src/com/biyi/hypnosis/activity/BaseActivity.java
+++ 3.0_Vistor/src/com/biyi/hypnosis/activity/BaseActivity.java
@@ -3,6 +3,11 @@ package com.biyi.hypnosis.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.biyi.hypnosis.R;
 
 /**
  * Description：
@@ -11,11 +16,36 @@ import android.support.v7.app.AppCompatActivity;
  * @author：ltc
  */
 public abstract class BaseActivity extends AppCompatActivity {
+    
+    private ImageView mBack;
+    private TextView mTextView;
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
         
+        mBack = findViewById(R.id.iv_back);
+        mTextView = findViewById(R.id.tv_title);
+    
+        if (mBack != null){
+            mBack.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+        
+                }
+            });
+        }
+        
+       
+    
     }
     abstract int  getLayoutId();
+    
+    public  void  setTittle (String tittle){
+        if (mTextView != null){
+            mTextView.setText(tittle);
+        }
+    }
 }
