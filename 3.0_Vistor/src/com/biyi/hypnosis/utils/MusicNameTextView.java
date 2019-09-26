e042d4959d6d7892af5da32d4a7555e94ecb2744
lanjiaojiao@xiaoneng.cn <lanjiaojiao>
2019-09-26 10:34:06 +0800

    update.

---
+++ 3.0_Vistor/src/com/biyi/hypnosis/utils/MusicNameTextView.java
@@ -0,0 +1,38 @@
+package com.biyi.hypnosis.utils;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+@SuppressLint("AppCompatCustomView")
+public class MusicNameTextView  extends TextView {
+
+    private LinearGradient mLinearGradient;
+    private Paint mPaint;
+    private int mViewWidth = 0;
+    private Rect mTextBound = new Rect();
+
+    public MusicNameTextView (Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mViewWidth = getMeasuredWidth();
+        mPaint = getPaint();
+        String mTipText = getText().toString();
+        mPaint.getTextBounds(mTipText, 0, mTipText.length(), mTextBound);
+        mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0,
+                new int[]{0xFFFFEABA, 0xFFBE8B49},
+                null, Shader.TileMode.REPEAT);
+        mPaint.setShader(mLinearGradient);
+        canvas.drawText(mTipText, getMeasuredWidth() / 2 - mTextBound.width() / 2, getMeasuredHeight() / 2 + mTextBound.height() / 2, mPaint);
+    }
+
+}
